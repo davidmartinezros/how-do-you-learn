e78fede96c55c4cb137ebe403251829beea965d0
David <david.martinez.etic@gmail.com>
2017-05-04 17:53:22 +0200

    put crossOrigin to the controller to have access from any domain

--- src/main/java/org/dmr/controllers/HowDYLController.java
+++ src/main/java/org/dmr/controllers/HowDYLController.java
@@ -8,6 +8,7 @@ import org.dmr.services.HowDYLService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,6 +28,7 @@ import io.reactivex.subscribers.ResourceSubscriber;
  */
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class HowDYLController {
 	
 	@Autowired
